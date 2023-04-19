69d968921e3241bc99a0137589992d45e1f9c397
Daniel Curtin <dcurtin.work@gmail.com>
2023-04-19 14:25:41 -0600

    Fix tests to run with HashRouter, add formatting

--- cypress/e2e/login_spec.cy.js
+++ cypress/e2e/login_spec.cy.js
@@ -1,29 +1,33 @@
 describe('Login Page', () => {
-    beforeEach(() => {
-    cy.intercept('GET', 'https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=JL2wo9KGenD4ya0vsuAP1IGxduivvAwS',{fixture: "lists.json"})
-    cy.visit('http://localhost:3000/login')
-})
-   it('Should be able to login', () => {
-       cy.get('#email').type('test@test.com')
-       cy.get('#pass').type('password')
-       cy.get('.submit').click()
-       cy.get('.users-name').should('contain', 'Hello, test testing!')
-    })
-    it('Should throw an error when we put in the wrong email', () => {
-        cy.get('#email').type('test')
-        cy.get('#pass').type('password')
-        cy.get('.submit').click()
-        cy.get('.error-code').should('contain', 'Invalid Username or Password')
-     })
-     it('Should throw an error when we put in the wrong password', () => {
-        cy.get('#email').type('test@test.com')
-        cy.get('#pass').type('pass')
-        cy.get('.submit').click()
-        cy.get('.error-code').should('contain', 'Invalid Username or Password')
-     })
-     it('Should throw an error when we dont enter user or password', () => {
-        cy.get('.submit').click()
-        cy.get('.error-code').should('contain', 'Invalid Username or Password')
-     })
    
-})
+   beforeEach(() => {
+      cy.intercept('GET', 'https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=JL2wo9KGenD4ya0vsuAP1IGxduivvAwS', { fixture: 'lists.json' })
+      .visit('http://localhost:3000/#/login');
+   });
+
+   it('Should be able to login', () => {
+      cy.get('#email').type('test@test.com')
+      .get('#pass').type('password')
+      .get('.submit').click()
+      .get('.users-name').should('contain', 'Hello, test testing!');
+   });
+
+   it('Should throw an error when we put in the wrong email', () => {
+      cy.get('#email').type('test')
+      .get('#pass').type('password')
+      .get('.submit').click()
+      .get('.error-code').should('contain', 'Invalid Username or Password');
+   });
+
+   it('Should throw an error when we put in the wrong password', () => {
+      cy.get('#email').type('test@test.com')
+      .get('#pass').type('pass')
+      .get('.submit').click()
+      .get('.error-code').should('contain', 'Invalid Username or Password');
+   });
+
+   it('Should throw an error when we dont enter user or password', () => {
+      cy.get('.submit').click()
+      .get('.error-code').should('contain', 'Invalid Username or Password');
+   });
+});
